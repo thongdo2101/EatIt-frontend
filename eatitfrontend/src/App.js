07f1d2bb00598ee32f23f144e7c43504bf04e094
daotuanbk <daotuanbk@gmail.com>
2018-05-20 13:32:29 +0700

    Commited

--- eatitfrontend/src/App.js
+++ eatitfrontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import DetailsScreen from './containers/DetailsScreen'
+import DetailsScreen from './containers/DetailsScreen';
+import SearchScreen from './containers/SearchScreen';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 class App extends Component {
