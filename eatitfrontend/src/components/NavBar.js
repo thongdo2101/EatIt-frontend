e5f2b3f43007b46c3e2e603bc3fd6937d891ec53
daotuanbk <daotuanbk@gmail.com>
2018-05-20 13:02:05 +0700

    updated details screen

--- eatitfrontend/src/components/NavBar.js
+++ eatitfrontend/src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import logo from "../img/logo2.png";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">
-      <img src="img/logo2.png" alt="" width="50%" />
-    </a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="" width="50%" />
+          </a>
 
-  </nav>
+        </nav>
       </div>
     );
   }
